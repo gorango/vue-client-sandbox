b1d43e62adf50358c0dacd46f0aaafcd604e9fb0
Goran <gospaso@gmail.com>
2019-02-17 22:48:23 -0500

    add quasar, yarn

--- src/main.js
+++ src/main.js
@@ -4,6 +4,18 @@ import router from "./router";
 import store from "./store";
 import "./registerServiceWorker";
 
+import "./styles/quasar.styl";
+import iconSet from "quasar-framework/icons/mdi";
+import "quasar-extras/animate";
+import "quasar-extras/roboto-font";
+import "quasar-extras/mdi";
+import Quasar from "quasar";
+
+Vue.use(Quasar, {
+  config: {},
+  iconSet: iconSet
+});
+
 Vue.config.productionTip = false;
 
 new Vue({
